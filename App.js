c736e6e150d63842f959021fd6020fd370d69c0c
somnath khadanga <somnathkhadanga@somnaths-MacBook-Pro.local>
2024-02-13 13:09:20 +0530

    send data to api

--- App.js
+++ App.js
@@ -5,26 +5,57 @@ import LiveAudioStream from 'react-native-live-audio-stream';
 import {check, PERMISSIONS, RESULTS, request} from 'react-native-permissions';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {Buffer} from 'buffer';
-import axios from 'axios';
 const options = {
   sampleRate: 32000, // default is 44100 but 32000 is adequate for accurate voice recognition
   channels: 1, // 1 or 2, default 1
   bitsPerSample: 16, // 8 or 16, default 16
   audioSource: 6, // android only (see below)
-  bufferSize: 4096, // default is 2048
+  bufferSize: 14096, // default is 2048
 };
 LiveAudioStream.init(options);
 
 export default function App() {
   const [isStreaming, setIsStreaming] = useState(false);
-  const [streamData, setStreamData] = useState([]);
 
   LiveAudioStream.on('data', data => {
     // base64-encoded audio data chunks
 
     const chunk = Buffer.from(data, 'base64');
+    console.log('====================================');
     console.log(chunk, 'chunk');
+    console.log('====================================');
+    sendAudioChunk(chunk);
   });
+
+  const sendAudioChunk = async chunk => {
+    try {
+      const response = await fetch(
+        'https://app-backend-pink.vercel.app/api/hello',
+        {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify({data: chunk}), // body data type must match "Content-Type" header
+        },
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP Error: Status ${response.status}`);
+      }
+
+      const data = await response.json(); // Changed from response.json() as await needed here
+      console.log('Received Recognition:', data);
+    } catch (error) {
+      console.error('Error sending audio chunk:', error);
+    }
+  };
+
   const handleStreamToggle = async () => {
     if (isStreaming) {
       LiveAudioStream.stop();
